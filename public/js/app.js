7c57ce5a88dc5ccd3d2cc0c4b0b4b03d9fd544ff
MenendezON <menendezon@gmail.com>
2023-05-02 21:36:19 +0000

    Create an empty class

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,10 @@ const menu = document.querySelectorAll('.item-menu');
 const h1 = document.querySelector('h1');
 const contain = document.querySelector('.contain');
 const currentDate = document.querySelector('.current-date p');
-let books = [];
+
+class ListOfBooks {}
+
+const listofbooks = new ListOfBooks();
 
 /* eslint-disable no-unused-vars */
 const menuActive = (menu, active) => {
@@ -10,52 +13,6 @@ const menuActive = (menu, active) => {
     menu[i].classList.remove('active');
   }
   active.classList.add('active');
-};
-
-const generateTable = () => {
-  contain.innerHTML = '';
-  const table = document.createElement('table');
-  // header
-  const tr = document.createElement('tr');
-  const th1 = document.createElement('th');
-  th1.textContent = '#';
-  const th2 = document.createElement('th');
-  th2.textContent = 'Title';
-  const th3 = document.createElement('th');
-  th3.textContent = 'Author';
-  const th4 = document.createElement('th');
-  th4.textContent = 'Created';
-  const th5 = document.createElement('th');
-  th5.textContent = 'Actions';
-  tr.appendChild(th1);
-  tr.appendChild(th2);
-  tr.appendChild(th3);
-  tr.appendChild(th4);
-  tr.appendChild(th5);
-  table.appendChild(tr);
-  // body
-  books.forEach((book, i) => {
-    const tr1 = document.createElement('tr');
-    const th11 = document.createElement('th');
-    th11.textContent = i + 1;
-    const th12 = document.createElement('td');
-    th12.textContent = book.title;
-    const th13 = document.createElement('td');
-    th13.textContent = book.author;
-    const th14 = document.createElement('td');
-    th14.textContent = book.created_at;
-    const th15 = document.createElement('td');
-    th15.innerHTML = `
-    <a href="#" onclick="popupBook(${i})"><i class="fa fa-search-plus" aria-hidden="true"></i></a> | 
-    <a href="#" onclick="removeBook(${i})"><i class="fa fa-trash-o" aria-hidden="true"></i></a>`;
-    tr1.appendChild(th11);
-    tr1.appendChild(th12);
-    tr1.appendChild(th13);
-    tr1.appendChild(th14);
-    tr1.appendChild(th15);
-    table.appendChild(tr1);
-  });
-  contain.appendChild(table);
 };
 
 const generateAddForm = () => {
@@ -72,6 +29,7 @@ const generateAddForm = () => {
   input.setAttribute('type', 'text');
   input.setAttribute('name', 'title');
   input.setAttribute('id', 'title');
+  input.setAttribute('value', 'valeur 1');
   input.setAttribute('placeholder', 'Write the title');
   l1.appendChild(input);
   const l2 = document.createElement('fieldset');
@@ -82,6 +40,7 @@ const generateAddForm = () => {
   input2.setAttribute('type', 'text');
   input2.setAttribute('name', 'author');
   input2.setAttribute('id', 'author');
+  input2.setAttribute('value', 'valeur 1');
   input2.setAttribute('placeholder', 'Write the author');
   l2.appendChild(input2);
   // input file
@@ -127,18 +86,12 @@ const generateContactForm = () => {
   contain.appendChild(parentElement);
 };
 
-const removeBook = (index) => {
-  books = books.filter((book) => book.title !== books[index].title);
-  localStorage.setItem('books', JSON.stringify(books));
-  generateTable();
-};
-
 const changeTitle = (itemMenu = '#page-0') => {
   let text = '';
   switch (itemMenu) {
     case '#page-0':
       text = 'All awesome books';
-      generateTable();
+      listofbooks.generateTable();
       break;
     case '#page-1':
       text = 'Add a new book';
@@ -184,16 +137,18 @@ window.addEventListener('DOMContentLoaded', (event) => {
   setInterval(generateCurrentDate, 1000);
   changeTitle();
   menuActive(menu, menu[0]);
-  if (localStorage.getItem('books')) {
-    books = JSON.parse(localStorage.getItem('books'));
-  }
 
-  generateTable();
+  listofbooks.books = JSON.parse(localStorage.getItem('datas')) ?? [];
+  listofbooks.generateTable();
 });
 
 const validateForm = (coverImg = '') => {
   const title = document.getElementById('title');
   const author = document.getElementById('author');
+
+  if (coverImg.trim().length === 0) {
+    coverImg = './public/images/default.png';
+  }
 
   const newBook = {
     title: title.value,
@@ -203,12 +158,9 @@ const validateForm = (coverImg = '') => {
     description: '',
   };
 
-  const success = books.push(newBook);
-  localStorage.setItem('books', JSON.stringify(books));
-  if (success >= books.length) {
+  if (listofbooks.addBook(newBook)) {
     title.value = '';
     author.value = '';
-    generateTable();
     return true;
   }
   return false;
@@ -225,6 +177,12 @@ const updloadImage = () => {
   }
 };
 
+const removeBook = (index) => {
+  listofbooks.removeBook(index);
+  listofbooks.getData();
+  listofbooks.generateTable();
+};
+
 const popupBook = (index) => {
   const back = document.createElement('div');
   back.classList.add('fullScreen');
@@ -232,11 +190,11 @@ const popupBook = (index) => {
   popup.classList.add('popup');
 
   const side0 = document.createElement('div');
-  side0.innerHTML = `<img src="${books[index].cover}" alt="">`;
+  side0.innerHTML = `<img src="${listofbooks.books[index].cover}" alt="">`;
   popup.appendChild(side0);
 
   const side1 = document.createElement('div');
-  side1.innerHTML = `<h2>${books[index].title}</h2><h3><em>by ${books[index].author}</em></h3><p><em>added ${books[index].created_at}</em></p><p>${books[index].description}</p>`;
+  side1.innerHTML = `<h2>${listofbooks.books[index].title}</h2><h3><em>by ${listofbooks.books[index].author}</em></h3><p><em>added ${listofbooks.books[index].created_at}</em></p><p>${listofbooks.books[index].description}</p>`;
   popup.appendChild(side1);
 
   const link = document.createElement('a');
